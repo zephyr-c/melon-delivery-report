77a1473637085b85cd41a56536064aeae9e7ecd1
Zephyr Worthington <z.worthington@outlook.com>
2019-10-03 20:09:12 -0700

    inital commit with function written but no changes to original code

---
+++ produce_summary.py
@@ -0,0 +1,59 @@
+def delivery_report(filename)
+    the_file = open(filename)
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+    the_file.close()
+
+
+
+
+print("Day 1")
+the_file = open("um-deliveries-20140519.txt")
+for line in the_file:
+    line = line.rstrip()
+    words = line.split('|')
+
+    melon = words[0]
+    count = words[0]
+    amount = words[0]
+
+    print("Delivered {} {}s for total of ${}".format(
+        count, melon, amount))
+the_file.close()
+
+
+print("Day 2")
+the_file = open("um-deliveries-20140520.txt")
+for line in the_file:
+    line = line.rstrip()
+    words = line.split('|')
+
+    melon = words[0]
+    count = words[0]
+    amount = words[0]
+
+    print("Delivered {} {}s for total of ${}".format(
+        count, melon, amount))
+the_file.close()
+
+
+print("Day 3")
+the_file = open("um-deliveries-20140521.txt")
+for line in the_file:
+    line = line.rstrip()
+    words = line.split('|')
+
+    melon = words[0]
+    count = words[0]
+    amount = words[0]
+
+    print("Delivered {} {}s for total of ${}".format(
+        count, melon, amount))
+the_file.close()
